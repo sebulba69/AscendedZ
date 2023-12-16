3a8a7a8d82eb393b2c199a691f91e6ee8c1d236d
sebulba69 <charlesengland455@gmail.com>
2023-12-16 15:34:28 -0800

    More updates
    More updates to sigil

--- screens/SigilAssets/SigilScreen.cs
+++ screens/SigilAssets/SigilScreen.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class SigilScreen : Control
 {
+	private VBoxContainer _sigilVBox;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        
+		_sigilVBox = this.GetNode<VBoxContainer>("%SigilVBox");   
     }
 }
