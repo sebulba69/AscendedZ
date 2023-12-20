bd3c3bacc62eba28302918fd92c8ec0110e51671
sebulba <72589301+sebulba69@users.noreply.github.com>
2023-12-20 08:04:58 -0800

    Changed game to full screen by default
    Full screen is now the default starting setting.

--- sigils/SigilUpgradeBar.cs
+++ sigils/SigilUpgradeBar.cs
@@ -12,13 +12,13 @@ public partial class SigilUpgradeBar : HBoxContainer
 
 	private int _sigilIndex;
 	private Button _levelUpButton, _deleteButton;
-    private ProgressBar _xpProgressBar;
+	private ProgressBar _xpProgressBar;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		this.AddUserSignal(READY_REMOVE);
-        
+		
 		_xpProgressBar = this.GetNode<ProgressBar>("%XPProgressBar");
 
 		_levelUpButton = this.GetNode<Button>("%LevelUpButton");
@@ -26,24 +26,24 @@ public partial class SigilUpgradeBar : HBoxContainer
 
 		_levelUpButton.Pressed += _OnLevelUpButtonClicked;
 		_deleteButton.Pressed += _OnDeleteButtonClicked;
-    }
+	}
 
 	public void Initialize(int sigilIndex)
 	{
-        _sigilIndex = sigilIndex;
-        GameObject gameObject = PersistentGameObjects.Instance();
+		_sigilIndex = sigilIndex;
+		GameObject gameObject = PersistentGameObjects.Instance();
 		BossSigil sigil = gameObject.MainPlayer.LockedSigils[_sigilIndex];
 	}
 
 	private void _OnLevelUpButtonClicked()
 	{
-        GameObject gameObject = PersistentGameObjects.Instance();
+		GameObject gameObject = PersistentGameObjects.Instance();
 		MainPlayer player = gameObject.MainPlayer;
 		Wallet wallet = player.Wallet;
 		BossSigil sigil = player.LockedSigils[_sigilIndex];
-        string auraKey = "Sigil Aura";
+		string auraKey = "Sigil Aura";
 
-        Currency sigilAura = wallet.Currency[auraKey];
+		Currency sigilAura = wallet.Currency[auraKey];
 		if(sigilAura.Amount - 1 > 0)
 		{
 			sigilAura.Amount--;
@@ -55,13 +55,13 @@ public partial class SigilUpgradeBar : HBoxContainer
 				this.EmitSignal(READY_REMOVE);
 			}
 		}
-    }
+	}
 
 	private void _OnDeleteButtonClicked()
 	{
-        MainPlayer player = PersistentGameObjects.Instance().MainPlayer;
+		MainPlayer player = PersistentGameObjects.Instance().MainPlayer;
 		BossSigil sigil = player.LockedSigils[_sigilIndex];
-        player.LockedSigils.Remove(sigil);
-        this.EmitSignal(READY_REMOVE);
-    }
+		player.LockedSigils.Remove(sigil);
+		this.EmitSignal(READY_REMOVE);
+	}
 }
