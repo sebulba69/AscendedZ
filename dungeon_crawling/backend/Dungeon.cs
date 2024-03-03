a1b92eea080a66307f88c0707f3fd58c9c389417
sebulba69 <charlesengland455@gmail.com>
2024-03-02 22:02:37 -0800

    Started work on Dungeons

---
+++ dungeon_crawling/backend/Dungeon.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscendedZ.dungeon_crawling.backend
+{
+    /// <summary>
+    /// 1 dungeon floor
+    /// </summary>
+    public class Dungeon
+    {
+        private int _maxTileCount = 12;
+        private int _tileCount = 1;
+        public int MaxTileCount { get => _maxTileCount; set => _maxTileCount = value; }
+        public int TileCount { get => _tileCount; set => _tileCount = value; }
+        public Tile Root { get; set; }
+        public Tile Exit { get; set; }
+        public Tile CurrentTile { get; set; }
+        public Dungeon()
+        {
+            if(Root == null)
+            {
+                Root = new Tile()
+                {
+                    Value = 0,
+                    Occupied = true
+                };
+
+                CurrentTile = Root;
+            }
+        }
+
+        public void MoveRight()
+        {
+            if(CurrentTile.Right == null)
+            {
+                if(TileCount < MaxTileCount)
+                {
+                    int value = CurrentTile.Value + 1;
+                    CurrentTile.Occupied = false;
+
+                    CurrentTile.Right = new Tile()
+                    {
+                        Value = value,
+                        Occupied = true
+                    };
+
+                    IncrementTileCount();
+                }
+            }
+            else
+            {
+                CurrentTile.Occupied = false;
+                CurrentTile = CurrentTile.Right;
+            }
+        }
+
+        private void IncrementTileCount()
+        {
+            TileCount++;
+            if (TileCount == MaxTileCount)
+                Exit = CurrentTile;
+        }
+        
+
+
+    }
+}
---
+++ dungeon_crawling/backend/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscendedZ.dungeon_crawling.backend
+{
+    /// <summary>
+    /// Dungeon Tile
+    /// </summary>
+    public class Tile
+    {
+        /// <summary>
+        /// The layer above other tiles this node is on
+        /// </summary>
+        public int Layer { get; set; }
+        public int Value { get; set; }
+        public Tile Left { get; set; }
+        public Tile Right { get; set; }
+        public bool Occupied { get; set; }
+        /// <summary>
+        /// Tiles on lower layers connected to this one
+        /// </summary>
+        public List<Tile> Connected { get; set; }
+
+        public Tile()
+        {
+            if (Connected == null)
+            {
+                Connected = new List<Tile>();
+                Occupied = false;
+            }
+        }
+    }
+}
