a6f04d2b47d88f7013e1fda0408e0203019c2ac1
sebulba69 <charlesengland455@gmail.com>
2024-07-20 16:40:29 -0700

    Refund button moved

--- .godot/mono/temp/obj/ExportDebug/win-x64/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportDebug/win-x64/Ascended Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa2af6c9b8fca498d7da924a12c69be805f66a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3e12eee40cd6519ca5a8746952291ea0ef614c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
