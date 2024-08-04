99c0d5b0a5b6a49bde790bc031a87bfb9d3a6f05
sebulba69 <charlesengland455@gmail.com>
2024-08-04 11:40:22 -0700

    Updated version number

--- screens/StartScreen.cs
+++ screens/StartScreen.cs
@@ -13,7 +13,7 @@ using System.Xml.Linq;
 
 public partial class StartScreen : Transitionable2DScene
 {
-	private readonly string VERSION = "Pre-Alpha v0.04.56";
+	private readonly string VERSION = "Pre-Alpha v0.04.57";
 
 	/// <summary>
 	/// Node that appears on the starting screen.
