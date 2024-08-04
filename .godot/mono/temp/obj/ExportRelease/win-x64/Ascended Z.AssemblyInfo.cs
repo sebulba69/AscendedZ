fca15be2ea37ca70a7575644993f3e72b939d2fc
sebulba69 <charlesengland455@gmail.com>
2024-08-04 07:26:49 -0700

    More useless gitignore stuff

--- .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234ef255504b7f63177d1aa2cb0a57f7d3667bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bbc078036e612c6dacbc39c0c88b2e80c49be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
