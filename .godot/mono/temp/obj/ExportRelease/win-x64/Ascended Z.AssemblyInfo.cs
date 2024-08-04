7a49b2ebca39fb35c6ba2d8ac6757a4b212cfb98
sebulba69 <charlesengland455@gmail.com>
2024-08-04 11:41:46 -0700

    Adding cash files to git ignore

--- .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba61d3d10693e0eda0246ee4aa27f53138aca86f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c0d5b0a5b6a49bde790bc031a87bfb9d3a6f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
