ba61d3d10693e0eda0246ee4aa27f53138aca86f
sebulba69 <charlesengland455@gmail.com>
2024-08-04 09:54:39 -0700

    Set effect to nothing by default in the RESET track, also more gitignore

--- .godot/mono/temp/obj/Debug/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Ascended Z.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c977db7c23ef51d696931fd6029d510c8388a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fd4cda8dd34c5f590ed80fe1f98decc5ff9060")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bbc078036e612c6dacbc39c0c88b2e80c49be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fd4cda8dd34c5f590ed80fe1f98decc5ff9060")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
