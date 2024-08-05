8a221d9a5a6d5bcacc6de1c95f8bf703f01c298d
sebulba69 <charlesengland455@gmail.com>
2024-08-04 19:08:51 -0700

    added caceh files to git ignore +dll files

--- .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportRelease/win-x64/Ascended Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a49b2ebca39fb35c6ba2d8ac6757a4b212cfb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492d51fc0b04dc1746ca8e877d8276da3a008c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ascended Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
